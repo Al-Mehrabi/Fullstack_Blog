b76b14d8568c63339b0698ba8f32f6b5e467da3b
Bleon5--Github--Username <bleonarllati2@gmail.com>
2025-03-31 00:59:20 +0200

    Finished home page ande create post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import PostDetails from "./PostDetails";
 import UpdatePost from "./UpdatePost";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
     <Router>
-      <div className="bg-sky-300 min-h-screen">
-        
-
+      <div className="bg-primary  min-h-screen ">
         <div className="container mx-auto p-6">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/post/:id" element={<PostDetails />} />
             <Route path="/update/:id" element={<UpdatePost />} />
+            <Route path="/createpost" element={<CreatePost />} />
           </Routes>
         </div>
       </div>
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
-
-
+import CardList from "./components/CardList";
 
 function Home() {
- 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center mb-6">Home </h1>
-      <div className="text-center mb-6">
-        <Link to={`/post/12`} className="bg-green-500 hover:bg-blue-700 text-white">
-         PostDetails
-        </Link>
+      <div className="text-center mb-6 flex justify-between place-items-center">
+        <h1 className="text-5xl font-bold text-white tracking-widest">Home</h1>
+        <div className="flex justify-center gap-3">
+          <Link
+            to={`/post/12`}
+            className="bg-black border text-white border-black font-bold py-3 px-6 rounded-3xl shadow-lg transition-transform duration-300 hover:translate-y-1 hover:shadow-2xl"
+          >
+            PostDetails
+          </Link>
+          <Link
+            to={`/createpost`}
+            className="bg-secondary text-white font-bold py-3 px-6 rounded-3xl shadow-lg transition-transform duration-300 hover:translate-y-1 hover:shadow-2xl"
+          >
+            Create Post
+          </Link>
+        </div>
       </div>
+      <h3 className="text-3xl font-bold text-white tracking-widest text-center mt-10 mb-10">
+        Posts
+      </h3>
+      <CardList />
     </div>
   );
 }
